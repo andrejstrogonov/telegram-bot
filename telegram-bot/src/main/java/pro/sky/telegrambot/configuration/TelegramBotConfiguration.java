0d7d133c64fae75aad69903f7101894f3a390688
andrejstrogonov <strogonovandrej@hotmail.com>
2025-05-29 21:14:56 +0300

    Added connection to PostgreSQL

--- telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -3,8 +3,11 @@ package pro.sky.telegrambot.configuration;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class TelegramBotConfiguration {
@@ -17,6 +20,15 @@ public class TelegramBotConfiguration {
         TelegramBot bot = new TelegramBot(token);
         bot.execute(new DeleteMyCommands());
         return bot;
+    }
+    @Bean
+    public DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .driverClassName("org.postgresql.Driver")
+                .url("jdbc:postgresql://localhost:5432/your_database_name")
+                .username("your_username")
+                .password("your_password")
+                .build();
     }
 
 }
