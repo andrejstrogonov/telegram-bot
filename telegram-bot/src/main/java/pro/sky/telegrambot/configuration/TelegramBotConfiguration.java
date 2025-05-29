3329d79707bdfcc34e6e2f180836eead258c278b
andrejstrogonov <strogonovandrej@hotmail.com>
2025-05-29 21:18:35 +0300

    Added /start message

--- telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -3,11 +3,8 @@ package pro.sky.telegrambot.configuration;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class TelegramBotConfiguration {
@@ -20,15 +17,6 @@ public class TelegramBotConfiguration {
         TelegramBot bot = new TelegramBot(token);
         bot.execute(new DeleteMyCommands());
         return bot;
-    }
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create()
-                .driverClassName("org.postgresql.Driver")
-                .url("jdbc:postgresql://localhost:5432/your_database_name")
-                .username("your_username")
-                .password("your_password")
-                .build();
     }
 
 }
--- telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ telegram-bot/src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,7 +29,10 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message() != null && "/start".equals(update.message().text())) {
+                long chatId = update.message().chat().id();
+                telegramBot.execute(new SendMessage(chatId, "Welcome to our Telegram Bot!"));
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
